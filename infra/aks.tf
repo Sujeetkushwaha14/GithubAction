9bfc4a3133082fc71cb37250df953a651ebb0908
sujeet kumar kushwaha <sujeetkushwaha14@gmail.com>
2025-05-27 18:53:10 +0530

    tested

---
+++ infra/aks.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "auto-rg"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "autodeployacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "autodeployaks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "autodeploy"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
